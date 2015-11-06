51c93fcac35ed15e74d7d39b63e9338b84d5591a
Jose Molina <jose.molina@cern.ch>
2015-11-06 11:57:43 +0100

    Added Splash fragment

--- app/src/main/java/ch/cern/cvmfs/activities/MainActivity.java
+++ app/src/main/java/ch/cern/cvmfs/activities/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 import ch.cern.cvmfs.cvmfs.R;
+import ch.cern.cvmfs.fragments.CVMFSFragment;
+import ch.cern.cvmfs.fragments.MainFragment;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,5 +13,27 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            loadMainFragment();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        CVMFSFragment mainFragment = getCurrentFragment(R.id.container);
+        if (mainFragment != null && mainFragment.backPressed()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    public void loadMainFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new MainFragment())
+                .commit();
+    }
+
+    protected CVMFSFragment getCurrentFragment(int idContainer) {
+        return (CVMFSFragment) getSupportFragmentManager().findFragmentById(idContainer);
     }
 }
