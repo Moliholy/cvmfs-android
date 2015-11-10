1f3c900fdd7700b55240e0f2781f78a7d5d173c2
Jose Molina <jmolinacolmenero@gmail.com>
2015-11-10 23:41:45 +0100

    Updated main activity

--- app/src/main/java/ch/cern/cvmfs/activities/MainActivity.java
+++ app/src/main/java/ch/cern/cvmfs/activities/MainActivity.java
@@ -2,16 +2,27 @@ package ch.cern.cvmfs.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+
+import java.io.File;
 
 import ch.cern.cvmfs.R;
 import ch.cern.cvmfs.fragments.CVMFSFragment;
 import ch.cern.cvmfs.fragments.MainFragment;
+import ch.cern.cvmfs.model.RepositoryManager;
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+	    File mainStorageDirectory = new File(RepositoryManager.CACHE_PATH);
+	    if (!mainStorageDirectory.exists()) {
+		    boolean result = mainStorageDirectory.mkdirs();
+		    if (!result) {
+			    Log.e("FileStorage", "Couldn't create the directory \'" + RepositoryManager.CACHE_PATH + "\'");
+		    }
+	    }
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             loadMainFragment();
@@ -21,7 +32,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public void onBackPressed() {
         CVMFSFragment mainFragment = getCurrentFragment(R.id.container);
-        if (mainFragment != null && mainFragment.backPressed()) {
+        if (mainFragment != null && mainFragment.onBackPressed()) {
             return;
         }
         super.onBackPressed();
