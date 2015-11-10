b49d53c429b7b678b03cf1a669699e60684a8177
Jose Molina <jmolinacolmenero@gmail.com>
2015-11-10 23:41:14 +0100

    Updated main fragment

--- app/src/main/java/ch/cern/cvmfs/fragments/MainFragment.java
+++ app/src/main/java/ch/cern/cvmfs/fragments/MainFragment.java
@@ -1,12 +1,15 @@
 package ch.cern.cvmfs.fragments;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import ch.cern.cvmfs.R;
 
@@ -14,7 +17,13 @@ import ch.cern.cvmfs.R;
 public class MainFragment extends CVMFSFragment implements SplashFragment.SplashListener {
 
     private Toolbar toolbar;
-    private View mView;
+	private View mView;
+	private TextView menuTitleTextView;
+	private ImageView drawerSwitcherImg;
+	private DrawerLayout drawerLayout;
+	private ImageView menuBackImg;
+	private ImageView menuLogoImageView;
+	private View mRightDrawerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,26 +33,66 @@ public class MainFragment extends CVMFSFragment implements SplashFragment.Splash
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        if (savedInstanceState == null) {
-            loadSplash(false);
-        }
         super.onViewCreated(view, savedInstanceState);
+	    prepareFragments();
+	    if (savedInstanceState == null) {
+		    loadSplash();
+	    }
     }
 
     @Override
-    protected void OnPrepareInterface() {
+    protected void onPrepareInterface() {
         toolbar = (Toolbar) mView.findViewById(R.id.main_toolbar);
-    }
+		mRightDrawerView = mView.findViewById(R.id.loggedin_main_right_frame);
+		drawerLayout = (DrawerLayout) mView.findViewById(R.id.drawer_content_layout);
+		drawerSwitcherImg = (ImageView) mView.findViewById(R.id.menu_drawer_btn);
+		drawerSwitcherImg.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (drawerLayout.isDrawerOpen(mRightDrawerView)) {
+					drawerLayout.closeDrawer(mRightDrawerView);
+				} else {
+					drawerLayout.openDrawer(mRightDrawerView);
+				}
+			}
+		});
+		menuBackImg = (ImageView) mView.findViewById(R.id.menu_back_btn);
+		menuTitleTextView = (TextView) mView.findViewById(R.id.menu_title_textview);
+		menuLogoImageView = (ImageView) mView.findViewById(R.id.menu_logo_imageview);
+		menuBackImg.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onBackPressed();
+			}
+		});
+	}
 
-    public void loadSplash(boolean recreateActivity) {
-        if (recreateActivity)
-            getActivity().recreate();
+	private void prepareFragments() {
+		replaceFragment(new DrawerFragment(), R.id.loggedin_main_right_frame);
+		drawerLayout.setDrawerListener(new ActionBarDrawerToggle(getActivity(),
+				drawerLayout, R.string.drawer_opened, R.string.drawer_closed));
+	}
+
+	@Override
+	public boolean onBackPressed() {
+		if (drawerLayout.isDrawerOpen(mRightDrawerView)) {
+			drawerLayout.closeDrawer(mRightDrawerView);
+			return true;
+		}
+		CVMFSFragment currentFragment = getCurrentFragment(R.id.main_container_frame);
+		return currentFragment != null && currentFragment.onBackPressed();
+	}
+
+    public void loadSplash() {
+        toolbar.setVisibility(View.GONE);
         replaceFragment(new SplashFragment(), R.id.main_container_frame);
     }
 
     private void loadRepositorySelection() {
-        toolbar.setVisibility(View.GONE);
-        replaceFragment(new RepositorySelectionFragment(), R.id.main_container_frame);
+        toolbar.setVisibility(View.VISIBLE);
+	    menuTitleTextView.setVisibility(View.GONE);
+	    menuLogoImageView.setVisibility(View.VISIBLE);
+	    replaceFragment(new RepositorySelectionFragment(), R.id.main_container_frame);
     }
 
     @Override
